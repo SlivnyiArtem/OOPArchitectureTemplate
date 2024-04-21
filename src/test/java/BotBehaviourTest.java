e553dfe508c66a276a00792050e7fd0ceb965372
flamberg <slivnay1965@gmail.com>
2024-04-22 02:36:57 +0500

    change expextedAnswer in test

--- src/test/java/BotBehaviourTest.java
+++ src/test/java/BotBehaviourTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.urfu.BotBehaviour;
 
@@ -8,7 +7,7 @@ public class BotBehaviourTest {
     public void makeAnswerStandardMessage(){
         BotBehaviour botBehaviour = new BotBehaviour();
         var actualAnswer = botBehaviour.makeAnswer("1", "Hello");
-        var expectedAnswer = "\"Ваше сообщение: 'привет'\"";
-        Assertions.assertEquals(actualAnswer, expectedAnswer);
+        var expectedAnswer = "\"Ваше сообщение: 'Hello'\"";
+        Assertions.assertEquals(expectedAnswer, actualAnswer);
     }
 }
