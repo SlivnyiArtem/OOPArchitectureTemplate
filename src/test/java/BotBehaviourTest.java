f623b83f4f106f70d0fe0700c5280ef2fc0bffb1
flamberg <slivnay1965@gmail.com>
2024-04-22 02:22:27 +0500

    step 2

--- src/test/java/BotBehaviourTest.java
+++ src/test/java/BotBehaviourTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.urfu.BotBehaviour;
+
 public class BotBehaviourTest {
+    @Test
+    public void makeAnswerStandardMessage(){
+        BotBehaviour botBehaviour = new BotBehaviour();
+        var actualAnswer = botBehaviour.makeAnswer("1", "Hello");
+        var expectedAnswer = "\"Ваше сообщение: 'привет'\"";
+        Assertions.assertEquals(actualAnswer, expectedAnswer);
+    }
 }
